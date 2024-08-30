373611738343714b5a07e813706a58a058343fa4
Hypkeh <144148068+Hypkeh@users.noreply.github.com>
2024-08-30 23:49:07 +0600

    Delete hwdjgo directory

--- hwdjgo/ManyToManyProject/manytomany_app/management/commands/courses_list.py
+++
@@ -1,7 +0,0 @@
-from django.core.management.base import BaseCommand
-from manytomany_app.models import Course
-
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        courses = Course.objects.all()
-        return courses
--- hwdjgo/ManyToManyProject/manytomany_app/management/commands/students_list.py
+++
@@ -1,7 +0,0 @@
-from django.core.management.base import BaseCommand
-from manytomany_app.models import Student
-
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        students = Student.objects.all()
-        return students
